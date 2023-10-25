fa236b391d610c96cf5ca2501afaee42b0b2a682
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-10-25 17:06:45 +0900

    feat: 유저 컨트롤러 / 유저 생성 Post 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe} from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/registerUser.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+    constructor(private userService: UserService){}
+
+    @Post('/signup')
+    signUp(@Body(ValidationPipe) registerUserDto: RegisterUserDto): Promise<void> {
+        return this.userService.signUp(registerUserDto)
+    }
+}

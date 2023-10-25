927910fcd8291ad7344b1e6d325d730d98183a28
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-10-25 17:05:41 +0900

    feat: 유저 서비스 / 유저 생성 함수 구현

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { RegisterUserDto } from './dto/registerUser.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+      @InjectRepository(UserRepository)
+      private userRepository: UserRepository,
+    ){}
+
+    async signUp(registerUserDto: RegisterUserDto): Promise<void>{
+        try {
+            return await this.userRepository.createUser(registerUserDto);
+        } catch (error) {
+            console.error(error);
+            throw new InternalServerErrorException();
+        }
+    }
+}

42f75159a8a223c069813daad9943989362b4660
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-10-25 17:53:05 +0900

    chore: app 모듈 / 안쓰는 import 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
-import { UserController } from './user/user.controller';
-import { PostController } from './post/post.controller';
-import { PostService } from './post/post.service';
-import { UserService } from './user/user.service';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
-import { AuthService } from './auth/auth.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 
@@ -19,7 +13,5 @@ import { PostModule } from './post/post.module';
     PostModule,
     AuthModule,
   ],
-  controllers: [UserController, PostController, AuthController],
-  providers: [PostService, UserService, AuthService],
 })
 export class AppModule {}

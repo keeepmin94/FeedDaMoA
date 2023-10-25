efd7be77fd0f0c9fb34fe14c9739f1471f62474e
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-10-25 09:45:18 +0900

    chore: 프로젝트 스켈레톤  환경세팅

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}

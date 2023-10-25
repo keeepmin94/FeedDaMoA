84ef7bae0595e5f1b883e15a84ad7ef39dbe6bd9
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-10-25 12:03:34 +0900

    choer: "user, post, tag의 컨트롤러, 서비스, 레포지토리 파일 생성"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { UserController } from './user/user.controller';
+import { PostController } from './post/post.controller';
+import { PostService } from './post/post.service';
+import { UserService } from './user/user.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
+    UserModule,
+    PostModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [UserController, PostController, AuthController],
+  providers: [PostService, UserService, AuthService],
 })
 export class AppModule {}

a9e7270360a97582ab045f9cefe71f937111faf1
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-10-25 12:18:33 +0900

    chore: auth 디렉터리, 모듈,  컨트롤러, 서비스 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,15 +6,19 @@ import { PostController } from './post/post.controller';
 import { PostService } from './post/post.service';
 import { UserService } from './user/user.service';
 import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
 import { AuthService } from './auth/auth.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
     UserModule,
     PostModule,
+    AuthModule,
   ],
   controllers: [UserController, PostController, AuthController],
   providers: [PostService, UserService, AuthService],

a646fd62251aebf7d9367643cec8f41d138f2def
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-10-25 17:52:04 +0900

    feat: auth 컨트롤러 / 로그인 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LogInDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authservice: AuthService){}
+
+  @Post('/signin')
+  signIn(@Body(ValidationPipe) logInDto: LogInDto): Promise<{ accessToken: string }> {
+    return this.authservice.signIn(logInDto)
+  }
+}

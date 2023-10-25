559d9e98d3eb9dc7437a462c4f0145d46af47cf2
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-10-25 17:05:00 +0900

    feat: 유저 레포지토리 유저 생성 구현

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -6,22 +6,26 @@ import * as bcrypt from 'bcryptjs'
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @CustomRepository(User)
-export class UserRepositroy extends Repository<User>{
-    async registerUser(registerUserDto: RegisterUserDto):Promise<void>{
-        const {username, password} = registerUserDto
+export class UserRepository extends Repository<User>{
+    async createUser(registerUserDto: RegisterUserDto): Promise<void> {
+      const {username, email, password, accepted} = registerUserDto
+      
+      const salt = await bcrypt.genSalt()
+      const hashedPassword = await bcrypt.hash(password, salt)
 
-        const salt = await bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(password, salt)
+      const user = this.create({
+        username, 
+        email, 
+        password: hashedPassword, 
+        accepted})
 
-        const user = this.create({username, password:hashedPassword})
-
-        try {
-            await this.save(user)
-        } catch (error) {
-            if (error.code === '23505') {
-                throw new ConflictException('존재하는 유저 이름입니다.')
-            }
-            throw new InternalServerErrorException()
+      try {
+          await this.save(user)
+      } catch (error) {
+        if (error.code === '23505') {
+          throw new ConflictException('존재하는 유저 이름입니다.')
         }
-    }
+        throw new InternalServerErrorException()
+    	}
+  }
 }

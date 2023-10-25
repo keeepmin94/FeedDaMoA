0f9916c335695328aa559c37f13294b9c25604b7
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-10-25 17:52:29 +0900

    feat: auth 서비스 / 로그인 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LogInDto } from './dto/login.dto';
+import { UserRepository } from 'src/user/user.repository';
+import * as bcrypt from 'bcryptjs'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+    private jwtService: JwtService
+  ) { }
+
+  async signIn(logIntDto: LogInDto): Promise<{accessToken: string}> {
+    const {username, password} = logIntDto
+    const user = await this.userRepository.findOne({where: {username}})
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // 유저 토큰 생성(Secret + Payload)
+      const payload = { username }
+      const accessToken = this.jwtService.sign(payload)
+      return { accessToken }
+    } else {
+      throw new UnauthorizedException('로그인 실패')
+    }
+  }
+}

d5ef4f0a0fc3491fe0202b311a7f187ccbf6dc79
keeepmin94 <wjdwkjh94@gmail.com>
2023-10-26 12:41:14 +0900

    refact: dev 브랜치 머지

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
-
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as config from 'config'
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,11 +12,11 @@ async function bootstrap() {
     .setDescription('The social API description')
     .setVersion('1.0')
     .addTag('user')
-    .build()
-  const document = SwaggerModule.createDocument(app, swaggerConfig)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
 
-  const serverConfig = config.get('server')
-  const port = serverConfig.port
+  const serverConfig = config.get('server');
+  const port = serverConfig.port;
   await app.listen(port);
   Logger.log(`Application running on port ${port}`);
 }

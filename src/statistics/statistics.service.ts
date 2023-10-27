d93b310051f87a6546cb8c12fda6dedb4eb4d7b7
keeepmin94 <wjdwkjh94@gmail.com>
2023-10-27 17:27:12 +0900

    refact: statistics 통계 빠진날 db에서 처리(todo)

--- src/statistics/statistics.service.ts
+++ src/statistics/statistics.service.ts
@@ -14,33 +14,11 @@ export class StatisticsService {
   async getStatisticsPost(
     statisticsDto: StatisticsDto,
   ): Promise<IStatisticsResult[]> {
-    // hashtag === '' ? 토큰으로 유저 계정 찾기 : hashtag
+    // TODO: hashtag === '' ? 토큰으로 유저 계정 찾기 : hashtag
 
     const result =
       await this.statisticsCustomRepository.getStatisticsPost(statisticsDto);
 
     return result;
   }
-
-  // private fillDate(statistics: any[], startDateTime: Date, endDateTime: Date) {
-  //   // 모든 시간대를 생성합니다.
-  //   const allDates = [];
-  //   const currentDate = new Date(startDateTime);
-  //   while (currentDate <= endDateTime) {
-  //     allDates.push(startDateTime);
-  //     currentDate.setDate(currentDate.getDate() + 1);
-  //   }
-
-  //   const result = allDates.map((date) => {
-  //     const d = date.toISOString().split('T')[0];
-  //     const c = statistics.find((row) => row.date === d);
-
-  //     return {
-  //       date: d,
-  //       count: c ? c.count : 0,
-  //     };
-  //   });
-
-  //   return result;
-  // }
 }

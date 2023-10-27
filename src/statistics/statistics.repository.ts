d381be093f29b6b615cbf8d6a8ecf3a1648c2b63
keeepmin94 <wjdwkjh94@gmail.com>
2023-10-27 17:26:10 +0900

    refact: statistics 원하는 통계값 if -> switch

--- src/statistics/statistics.repository.ts
+++ src/statistics/statistics.repository.ts
@@ -22,13 +22,32 @@ export class StatisticsCustomRepository extends Repository<Post> {
           "TO_CHAR(post.created_at, 'YYYY-MM-DD HH24') AS hour",
         );
 
-    value === StatisticsValue.COUNT
-      ? queryBuilder.addSelect('COUNT(*)', 'count')
-      : value === StatisticsValue.VIEW_COUNT
-      ? queryBuilder.addSelect('SUM(post.view_count)', 'count')
-      : value === StatisticsValue.LIKE_COUNT
-      ? queryBuilder.addSelect('SUM(post.like_count)', 'count')
-      : queryBuilder.addSelect('SUM(post.share_count)', 'count');
+    // fix: Switch문이 더 좋은것 같아 수정
+    // value === StatisticsValue.COUNT
+    //   ? queryBuilder.addSelect('COUNT(*)', 'count')
+    //   : value === StatisticsValue.VIEW_COUNT
+    //   ? queryBuilder.addSelect('SUM(post.view_count)', 'count')
+    //   : value === StatisticsValue.LIKE_COUNT
+    //   ? queryBuilder.addSelect('SUM(post.like_count)', 'count')
+    //   : queryBuilder.addSelect('SUM(post.share_count)', 'count');
+
+    switch (value) {
+      case StatisticsValue.COUNT:
+        queryBuilder.addSelect('COUNT(*)', 'count');
+        break;
+      case StatisticsValue.VIEW_COUNT:
+        queryBuilder.addSelect('SUM(post.view_count)', 'count');
+        break;
+      case StatisticsValue.LIKE_COUNT:
+        queryBuilder.addSelect('SUM(post.like_count)', 'count');
+        break;
+      case StatisticsValue.SHARE_COUNT:
+        queryBuilder.addSelect('SUM(post.share_count)', 'count');
+        break;
+      default:
+        queryBuilder.addSelect('COUNT(*)', 'count'); // 혹시 몰라 default값으로
+        break;
+    }
 
     queryBuilder
       .where('tag.tag = :hashtag', { hashtag })

57a96d6788ad60fe8bbe2d6a8973fb17fac92e8c
[AI7/2반] 이은석 <zhes4593@gmail.com>
2023-10-29 20:15:46 +0900

    docs: post - 개별 포스트 조회, like, share 스웨거 추가

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Query, Param, Patch } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostValidationPipe } from './pipes/postValidation.pipe';
 import { PostDto } from './dto/post.dto';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 
 @Controller('posts')
 export class PostController {
@@ -17,16 +18,22 @@ export class PostController {
     return await this.postService.getPosts(postDto);
   }
 
+  @ApiOperation({ summary: 'post의 모든 필드 반환' })
+  @ApiParam({ name: 'id', description: 'Post ID' })
   @Get(':id')
   async getPostDetail(@Param('id') id: string): Promise<object> {
     return await this.postService.getPostDetail(id);
   }
 
+  @ApiOperation({ summary: '좋아요 : post의 like count 수정' })
+  @ApiParam({ name: 'id', description: 'Post ID' })
   @Patch(':id/like')
   async patchLikeCount(@Param('id') id: string): Promise<object> {
     return await this.postService.like(id);
   }
 
+  @ApiOperation({ summary: '공유 : post의 share count 수정' })
+  @ApiParam({ name: 'id', description: 'Post ID' })
   @Patch(':id/share')
   async patchShareCount(@Param('id') id: string): Promise<object> {
     return await this.postService.share(id);

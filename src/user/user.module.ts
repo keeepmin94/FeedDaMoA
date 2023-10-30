377783d609b48519cec664ea55f36ab54c28e5c4
[AI7/2반] 허창원 <wonn22@gmail.com>
2023-10-30 12:50:53 +0900

    chore: 불필요한 import문 삭제

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,23 +4,9 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmExModule } from '../common/decorator/typeorm-ex.module';
 import { JwtStrategy } from '../auth/jwt.strategy';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import * as config from 'config';
-
-const jwtConfig = config.get('jwt');
 
 @Module({
-  imports: [
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || jwtConfig.secret,
-      signOptions: {
-        expiresIn: jwtConfig.expiresIn,
-      },
-    }),
-    TypeOrmExModule.forCustomRepository([UserRepository]),
-  ],
+  imports: [TypeOrmExModule.forCustomRepository([UserRepository])],
   controllers: [UserController],
   providers: [UserService, JwtStrategy],
   exports: [TypeOrmExModule],
